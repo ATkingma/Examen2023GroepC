56cf4a6b70ecd2393bca03c018c4b8badc622aa7
ATkingma <60423088+ATkingma@users.noreply.github.com>
2023-02-15 15:25:59 +0100

    saves settings

---
+++ GroepC_UnityProject/Assets/Scripts/Utilities/DontDestroyOnLoad.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GroepC.Utilities
+{
+    /// <summary>
+    /// An class that contains an awake with dondestroyonload function being called.
+    /// </summary>
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+    }
+}
