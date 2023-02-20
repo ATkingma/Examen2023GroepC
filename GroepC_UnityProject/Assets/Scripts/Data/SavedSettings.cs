e67f91206031540f3d34bacfa0fef24786539d74
ATkingma <keizerxjwz4@gmail.com>
2023-02-20 13:36:11 +0100

    updated sumarys!

---
+++ GroepC_UnityProject/Assets/Scripts/Data/SavedSettings.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+namespace GroepC.Data
+{
+    /// <summary>
+    /// An class that can be saved as json.
+    /// </summary>
+    [Serializable]
+    public class SavedSettings
+    {
+        public SavedSettings()
+        {
+            MasterVolume = 0;
+            MusicVolume = 0;
+            SoundEffects = 0;
+            UIVolume = 0;
+            IsFullScreen = true;
+            SavedResolution = Screen.currentResolution;
+        }
+
+        /// <summary>
+        /// The saved volume of the master Volume
+        /// </summary>
+        public float MasterVolume;
+
+        /// <summary>
+        /// The saved volume of the music Volume
+        /// </summary>
+        public float MusicVolume;
+
+        /// <summary>
+        /// The saved volume of the sound effects
+        /// </summary>
+        public float SoundEffects;
+
+        /// <summary>
+        /// The saved volume of the UI Volume
+        /// </summary>
+        public float UIVolume;
+
+        /// <summary>
+        /// A saved bool that shows if the player is fulscreen
+        /// </summary>
+        public bool IsFullScreen;
+
+        /// <summary>
+        /// The saved Resolution
+        /// </summary>
+        public Resolution SavedResolution;
+    }
+}
--- GroepC_UnityProject/Assets/Scripts/Music/MusicPlayer.cs
+++ GroepC_UnityProject/Assets/Scripts/Music/MusicPlayer.cs
@@ -1,56 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Music player holds all the behaviour for the background.
-/// </summary>
-public class MusicPlayer : MonoBehaviour
+namespace GroepC.Music
 {
-
     /// <summary>
-    /// The audiosource of the music player this is the audioplayer where the audio wil be played on.
+    /// Music player holds all the behaviour for the background.
     /// </summary>
-    [SerializeField]
-    private AudioSource audioSource;
-
-    /// <summary>
-    /// All the audio clips that the music player can play.
-    /// </summary>
-    [SerializeField]
-    private AudioClip[] audioClips;
-
-    /// <summary>
-    /// The index of the current hold clip out of the audioclips list.
-    /// </summary>
-    private int currentClip = 0;
-
-    void Start()
+    public class MusicPlayer : MonoBehaviour
     {
-        audioSource = GetComponent<AudioSource>();
-        PlayNextClip();
-    }
 
-    void Update()
-    {
-        if (!audioSource.isPlaying)
+        /// <summary>
+        /// The audiosource of the music player this is the audioplayer where the audio wil be played on.
+        /// </summary>
+        [SerializeField]
+        private AudioSource audioSource;
+
+        /// <summary>
+        /// All the audio clips that the music player can play.
+        /// </summary>
+        [SerializeField]
+        private AudioClip[] audioClips;
+
+        /// <summary>
+        /// The index of the current hold clip out of the audioclips list.
+        /// </summary>
+        private int currentClip = 0;
+
+        void Start()
         {
+            audioSource = GetComponent<AudioSource>();
             PlayNextClip();
         }
-    }
 
-    /// <summary>
-    /// Plays the next clip and adds currentClip.
-    /// </summary>
-    void PlayNextClip()
-    {
-        if (currentClip >= audioClips.Length)
+        void Update()
         {
-            currentClip = 0;
+            if (!audioSource.isPlaying)
+            {
+                PlayNextClip();
+            }
         }
 
-        audioSource.clip = audioClips[currentClip];
-        audioSource.Play();
-        currentClip++;
+        /// <summary>
+        /// Plays the next clip and adds currentClip.
+        /// </summary>
+        void PlayNextClip()
+        {
+            if (currentClip >= audioClips.Length)
+            {
+                currentClip = 0;
+            }
+
+            audioSource.clip = audioClips[currentClip];
+            audioSource.Play();
+            currentClip++;
+        }
     }
 }
