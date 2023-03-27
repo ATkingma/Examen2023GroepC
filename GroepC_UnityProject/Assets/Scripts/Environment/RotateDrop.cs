231154efba3a190ab3a464a7bfa08743330a3c2a
JkredietW <93643451+JkredietW@users.noreply.github.com>
2023-03-27 09:08:59 +0200

    rotate adjustments
    Air-fan rotates

--- GroepC_UnityProject/Assets/Scripts/Environment/RotateDrop.cs
+++ GroepC_UnityProject/Assets/Scripts/Environment/RotateDrop.cs
@@ -8,8 +8,23 @@ namespace GroepC.Environment
     public class RotateDrop : MonoBehaviour
     {
         /// <summary>
+        /// Axis.
+        /// </summary>
+        public bool x, y, z;
+
+        /// <summary>
         /// Rotates.
         /// </summary>
-        private void Update() => transform.Rotate(Vector3.up, -90 * Time.deltaTime);
+        private void Update()
+        {
+            if(x)
+                transform.Rotate(Vector3.up, -90 * Time.deltaTime);
+            else if(y)
+                transform.Rotate(Vector3.right, -90 * Time.deltaTime);
+            else if(z)
+                transform.Rotate(Vector3.forward, -90 * Time.deltaTime);
+            else
+                transform.Rotate(Vector3.up, -90 * Time.deltaTime);
+        }
     }
 }
